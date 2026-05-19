2ff87f85060b4bf1f356b163891f63fe1262d764
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-05-19 12:34:39 -0500

    Update logger.py

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,1 +1,13 @@
+import logging
 
+def get_logger():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler("logs/app.log", encoding="utf-8"),
+            logging.StreamHandler()
+        ]
+    )
+
+    return logging.getLogger("engine")
