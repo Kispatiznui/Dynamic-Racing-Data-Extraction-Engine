392c65bbb4b7d31dc900a837e8efc5be2016eaf5
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-05-19 12:38:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from src.browser.login import login
 from src.browser.navigator import navigate_to_stats
 from src.scraper.rpr_extractor import extract_rpr_data
 from src.export.excel_exporter import export_to_excel
+from src.core.logger import get_logger
+
+logger = get_logger()
 
 def main():
     username = input("Username: ")
@@ -21,6 +24,8 @@ def main():
         all_data.extend(data)
 
     export_to_excel(all_data)
+
+    logger.info("Export completed")
 
     driver.quit()
 
