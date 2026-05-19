9f3085db6c3da2aee7c6af04133e5b4af3295e27
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-05-19 12:36:40 -0500

    Update waits.py

--- src/browser/waits.py
+++ src/browser/waits.py
@@ -1,1 +1,7 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+def wait_for(driver, by, selector, timeout=15):
+    return WebDriverWait(driver, timeout).until(
+        EC.presence_of_element_located((by, selector))
+    )
