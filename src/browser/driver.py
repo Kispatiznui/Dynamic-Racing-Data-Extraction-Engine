8381b0aa0764a4fc998a8f20dd9fdf80f5548fe0
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-05-19 12:31:43 -0500

    Update driver.py

--- src/browser/driver.py
+++ src/browser/driver.py
@@ -1,1 +1,13 @@
+from selenium import webdriver
 
+def create_driver():
+    options = webdriver.ChromeOptions()
+
+    # Puedes activar esto luego para VPS
+    # options.add_argument("--headless")
+
+    options.add_argument("--start-maximized")
+
+    driver = webdriver.Chrome(options=options)
+
+    return driver
